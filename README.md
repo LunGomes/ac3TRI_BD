0860dd728423d4e29cb7f7ed3a3360cf2fa6c9fe
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-31 13:30:36 -0300

    Luana Silva - AC de INW e LP2 - exemplo

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # ac3TRI_BD
+
+reinstale os programas após clonar o projeto 
+
+npm install
+
+testar o post no postman 
+{
+    "nome" : "Maria",
+    "email" : "Maria@gmail.com",
+    "endereco" : "Rua augusta",
+    "numero" : 100,
+    "cep" : "0012-9241",
+    "nascimento" : "12-12-2023"
+}
+
+codigos q usa no mongodb em ordem
+
+show dbs -> tem que mostrar o seu e copiar e colocar no arquivo
+
+use [banco que ele deu]
+
+show collections
+
+db.[banco que ele deu].find()
+
--- revAC2MIA.js
+++ revAC2MIA.js
@@ -1,36 +1,60 @@
-//instalando programas 
+//instalando programas
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-//configurando o roteamento para teste 
+
+//configurando o roteamento para teste no postman
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 const port = 3000;
 
+
 //configurando o acesso ao mongodb
-//o nome do schema é o nome da MODEL
+mongoose.connect('mongodb://127.0.0.1:27017/revAC2MIA',
+{   useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS : 20000
+});
+
+
+//criando a model/collection do seu projeto
 const PessoaSchema = new mongoose.Schema({
     nome : {type : String},
     email : {type : String, required : true},
-    endereco : {type : String},
+    endereco : { type : String},
     numero : {type : Number},
     cep : {type : String, required : true},
     nascimento : {type : Date, required : true}
 });
 
+
 const Pessoa = mongoose.model("Pessoa", PessoaSchema);
 
-//confirando os roteamentos
+
+//configurando os roteamentos
 app.post("/cadastropessoa", async(req, res)=>{
     const nome = req.body.nome;
     const email = req.body.email;
     const endereco = req.body.endereco;
     const numero = req.body.numero;
-    const cep = req.body.cep;
+    const cep  = req.body.cep;
     const nascimento = req.body.nascimento
-    
+
+    //testando se todos os campos foram preenchidos
+    if(nome == null || email == null || endereco == null || numero == null || cep == null || nascimento == null){
+        return res.status(400).json({error : "Preencher todos os campos"});
+    } 
+
+    //teste mais importante da ac 
+    const emailExiste = await Pessoa.findOne({email:email});
+
+    if(emailExiste){
+        return res.status(400).json({error : "Esse email já está registrado no sistema."});
+    }
+
+    //como fica no postman pra add 
     const pessoa = new Pessoa({
         nome : nome,
         email : email,
@@ -40,20 +64,28 @@ app.post("/cadastropessoa", async(req, res)=>{
         nascimento : nascimento
     })
 
+
     try{
         const newPessoa = await pessoa.save();
-
         res.json({error : null, msg : "Cadastro ok", pessoaId : newPessoa._id});
     } catch(error){
         res.status(400).json({error});
     }
+
 });
 
+//rota para o get de cadastro.html
+app.get("/cadastropessoa", async(req, res)=>{
+    res.sendFile(__dirname +"/cadastropessoa.html");
+});
+
+//rota raiz - inicio do inw por causa da pág html
 app.get("/", async(req, res)=>{
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname +"/index.html");
 });
 
-//configurando a porta
+
+//configurando a porta - pra ler que vc ta usando a porta 3000 no mongo e no postman
 app.listen(port, ()=>{
-    console.log(`servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
